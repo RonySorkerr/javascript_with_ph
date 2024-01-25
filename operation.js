25411e042db309a09560f6fa0029ce43d1cab532
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-25 23:02:52 +0600

    operations methods are common to me now

---
+++ operation.js
@@ -0,0 +1,17 @@
+var eggPrice = 10;
+var onionPrice = 20;
+
+console.log(eggPrice + onionPrice);
+console.log(eggPrice - onionPrice);
+console.log(eggPrice * onionPrice);
+console.log(onionPrice / eggPrice);
+
+console.log(12/5);
+console.log(12%5);
+console.log(9%2);
+console.log(11%6);
+// reminder or what the number stays after dividing the number.
+
+var money = 550;
+money = money + 12.5;
+console.log(money);
