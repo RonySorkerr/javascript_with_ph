e24ee724dfa7d7c5e7c5b5fc387beaa77ad444ae
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-25 22:50:52 +0600

    number class

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 // var age = 45;
 // var name = 'md.rony sorker';
 // console.log(age , name);
+
+var number = parseInt('69');
+console.log(number);
+
+var numberFloat = parseFloat(69.69);
+console.log(numberFloat)
+
+var num1 = 0.1; 
+var num2 = 0.2;
+var total = num1 + num2;
+console.log(total);
+// to fix the numbers after a floating point we can fix it with the systex is . toFixed
+console.log(total.toFixed(4));
+// we can say how many digits we want to show in our output.
+
