a8f4a3735b0ce5a719efed1efddeef7312e56475
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-25 23:17:24 +0600

    operation and concetanaiton done

--- operation.js
+++ operation.js
@@ -15,3 +15,23 @@ console.log(11%6);
 var money = 550;
 money = money + 12.5;
 console.log(money);
+
+
+var fridge = 20050;
+fridge -= 550; // substract the value of this var from the upper position.
+console.log(fridge);
+
+var bat = 1500;
+bat += 250; // add the value of this var from the upper position.
+console.log(bat);
+
+var komola = 25;
+komola *= 2; //  add the value of this var from the upper position.
+console.log(komola);
+
+//-----------------------------
+//concatanation
+var firstName = 'MObarok';
+var lastName = ' hosen';
+var name = firstName + lastName;
+console.log(name);
